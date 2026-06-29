3f673364fe5332641e8a3859740885347eabb253
Rithish Janjeerapu <100933061+Rithishkumar7@users.noreply.github.com>
2026-06-29 23:13:20 +0530

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low=0,high=0;
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                low++;
+                high++;
+            }
+            else if(c==')')
+            {
+                high--;
+                low--;
+            }
+            else{
+                low--;
+                high++;
+            }
+           if(low<0)
+        low=0;
+        if(high<0)
+        return false;
+        }
+        return low==0;
+        
+    }
+     
+        
+}
